5bcff6b1649bed8a0abcbf24a70394188a850d78
Rob Schwitzer <deadinthewater@me.com>
2018-06-19 03:22:34 +0000

    first commit

--- planning.rb
+++ planning.rb
@@ -1,25 +1,22 @@
 # Classes
 
-#         Game
-#           has:
-#               players
-#               turns
-#               start
-#               end
+#     Game
+#       has:
+#           players
+#           turns
+#           start
+#           end
 
+#     Player
+#       has:
+#           id
+#           3 lives
 
-#         Player
-#           has:
-#               id
-#               3 lives
+#     Turn
+#       has:
+#           question
+#           gets.chomp for answer
 
-
-#         Turn
-#           has:
-#               question
-#               gets.chomp for answer
-
-
-#         Questions
-#           has:
-#               function to make simple math question with random numbers
+#     Questions
+#       has:
+#           function to make simple math question with random numbers
